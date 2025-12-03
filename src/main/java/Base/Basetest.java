a07c72829bbb2c8da908aef5c0fec68e694f4214
supriyamane28 <supriya2888.mane@gmail.com>
2025-12-03 16:49:34 +0530

    Overwrite filename.txt with updated contentlogs/application.lo

--- src/main/java/Base/Basetest.java
+++ src/main/java/Base/Basetest.java
@@ -3,13 +3,23 @@ package Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 
 import utils.Log;
 
 public class Basetest {
 	
 	protected WebDriver driver;
+	
+	
+	
+	
 	
 	@BeforeMethod
 	public void setUp()
--- src/test/java/tests/Logintest.java
+++ src/test/java/tests/Logintest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import Base.Basetest;
 import Pages.Loginpage;
+
 import utils.Log;
 
 public class Logintest extends Basetest{
@@ -14,19 +15,25 @@ public class Logintest extends Basetest{
 	{
 		
 		Log.info("starting login test....");
+		
 		Loginpage loginpageobject=new Loginpage(driver);
 		
 		Log.info("adding creditials ...");
+		
 		loginpageobject.enterusername("admin@yourstore.com");
 		loginpageobject.enterpassword("admin");
+	
 		loginpageobject.clickonlogin();
 		
 		System.out.println(driver.getTitle());
 		
 		
 		Log.info("verifying page title ...");
+		
 	Assert.assertEquals(driver.getTitle(),"Just a moment...");
 		
+	
+	
 		
 				
 	}
